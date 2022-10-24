1183e46dc508289932f81b64649e8f7582cb691d
Каримуллин Семен Сергеевич <karimullin.ss@edu.spbstu.ru>
2022-10-25 00:38:02 +0300

    10.25.22

---
+++ Chapter 1/1.6.c
@@ -0,0 +1,8 @@
+// Program 1.6 A Simple C Program – Important
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Be careful!!\n\a"); // \a - use computer speakers
+    return 0;
+}
---
+++ Chapter 1/1.7.c
@@ -0,0 +1,16 @@
+// Program 1.7 Use a trigraph sequence
+// You need to use -trigraphs to enable them
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Use \?\?= to use ??= character\n");
+    printf("Use \?\?' to use ??' character\n");
+    printf("Use \?\?( to use ??( character\n");
+    printf("Use \?\?< to use ??< character\n");
+    printf("Use \?\?! to use ??! character\n");
+    printf("Use \?\?> to use ??> character\n");
+    printf("Use \?\?) to use ??) character\n");
+    printf("Use \?\?- to use ??- character");
+    return 0;
+}
